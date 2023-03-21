7badc4aeb21c5285f8ea92d2d568631c3e8b8197
Kiuk Chung <kiukchung@gmail.com>
2023-03-21 17:58:41 +0000

    Added configs for AL2

---
+++ bin/setup/install_desktop_al2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+# installs linux GUI on Amazon Linux 2
+
+set -o pipefail
+
+echo "Installing X11 Fonts..."
+yum install -y xorg-x11-fonts-Type1
+
+echo "Installing XFCE..."
+wget http://archives.fedoraproject.org/pub/archive/fedora/linux/releases/23/Everything/x86_64/os/Packages/d/desktop-backgrounds-basic-23.0.0-1.fc23.noarch.rpm
+rpm -Uvh desktop-backgrounds-basic-23.0.0-1.fc23.noarch.rpm
+
+wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -Uvh epel-release-latest-7.noarch.rpm
+
+# install vnc
+echo "Installing vnc server..."
+yum install -y tigervnc-server
