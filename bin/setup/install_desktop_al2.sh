32841a4239151fa96ef19c9c9e582979ca9172de
Kiuk Chung <kiukchung@gmail.com>
2023-07-11 16:44:47 +0000

    update

--- bin/setup/install_desktop_al2.sh
+++ bin/setup/install_desktop_al2.sh
@@ -4,7 +4,7 @@
 set -o pipefail
 
 echo "Installing X11 Fonts..."
-yum install -y xorg-x11-fonts-Type1
+yum install -y xorg-x11-fonts-Type1 xorg-x11-fonts-base libXfont
 
 echo "Installing XFCE..."
 wget http://archives.fedoraproject.org/pub/archive/fedora/linux/releases/23/Everything/x86_64/os/Packages/d/desktop-backgrounds-basic-23.0.0-1.fc23.noarch.rpm
