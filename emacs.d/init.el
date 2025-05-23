77c610278e089de41453ab57420a9c91c3e0df12
Kiuk Chung <kiuk@google.com>
2025-05-23 09:57:43 -0700

    update

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,7 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (load-theme 'zenburn t)
 (setq vc-follow-symlinks t)
+(setq-default indent-tabs-mode nil)
+(setq auto-save-default nil)
+(setq make-backup-files nil)
+(setq create-lockfiles nil)
